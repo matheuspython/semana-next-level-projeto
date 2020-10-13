a39dd773e0d212c217dc4e385b4c63b6840b4f87
matheus souza <ruivorobado@gmail.com>
2020-10-13 13:55:20 -0300

    arrumando o mapa corrigindo a localizacao icones mensagens e tambem altura e largura dos icones

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 //document.body.style.background  |  retorna a cor do background do elemento todo//
 //document.body.style.background = 'red' | o funto recebeu a cor vermelha//
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([-27.222633, -49.6455874], 15);
 //adicionando var com args de latitude longitude e zoom//
 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+
+
+const icon = L.icon({
+    iconUrl: "../public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAncor: [29, 68],
+    popupAnchor: [170, 2]
+})
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="../public/images/arrow-white.svg"></a>')
+
+
+
+L.marker([-27.222633, -49.6455874], { icon }).addTo(map)
+    .bindPopup(popup)
+
 //imgagem do mapa + popup//
